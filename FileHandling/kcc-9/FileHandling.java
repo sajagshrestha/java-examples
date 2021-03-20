264e3975be163c62f7b01a2ec503f19c98b4bf1e
sajagshrestha <sajagshrestha0852@gmail.com>
2021-03-20 19:49:46 +0545

    comments

--- FileHandling/kcc-9/FileHandling.java
+++ FileHandling/kcc-9/FileHandling.java
@@ -19,7 +19,7 @@ class FileHandling{
 			FileReader file2 = new FileReader("./test.txt");
 			int characterCount  = file2.read(); //returns -1 at the end of line
 			while(characterCount != -1){
-				System.out.print((char)characterCount);//typecas to character from ASCII
+				System.out.print((char)characterCount);//typecast to character from ASCII
 				characterCount = file2.read();//read file again to check end of the line
 			}
 			file2.close();
