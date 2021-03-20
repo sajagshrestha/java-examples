2626df571800ac1f55beb90bd3f06a44bc6df944
sajagshrestha <sajagshrestha0852@gmail.com>
2021-03-20 14:26:13 +0545

    kcc no.6 added

---
+++ MultiThreading/kcc-6/Greetings.java
@@ -0,0 +1,37 @@
+//thread 1 class
+class Thread1 extends Thread{
+	public void run(){
+		while(true){
+			try{
+				Thread.sleep(1000);
+			}
+			catch(Exception e){
+				System.out.println(e);
+			}
+			System.out.println("good morning");
+		}
+	}
+}
+//thread 2 class
+class Thread2 extends Thread{
+	public void run(){
+		while(true){
+			try{
+				Thread.sleep(2000);
+			}
+			catch(Exception e){
+				System.out.println(e);
+			}
+			System.out.println("good afternoon");
+		}
+	}
+}
+//main class
+class Greetings{
+	public static void main(String args[]){
+		Thread1 t1 = new Thread1();
+		Thread2 t2 = new Thread2();
+		t1.start();
+		t2.start();
+	}
+}
